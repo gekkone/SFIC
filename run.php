631beedcd352187c8cd719ca4c99e947efc07e2e
Gekkone <sergej.soloyov@yandex.ru>
2020-09-11 19:24:27 +0300

    Изменена логика ротации логов Логи теперь имеют в названии текущую дату

--- run.php
+++ run.php
@@ -6,34 +6,61 @@ use SFIC\SficScan;
 
 require 'vendor/autoload.php';
 
-log_rotate('SFIC.log', 5 * pow(10, 6), 2);
+log_rotate('logs', 2);
+$logFile = 'logs/SFIC_' . date('d-m-Y') . '.log';
 $logger = new Logger('SFIC');
-$logger->pushHandler(new StreamHandler('SFLIC.log', Logger::INFO));
+$logger->pushHandler(new StreamHandler($logFile, Logger::INFO));
 $config = include 'config.php';
 
 $scaner = new SficScan($config, $logger);
 $scaner->scan();
 
 /**
- * @param string $filename
- * @param int $size
+ * @param string $logsDir
  * @param int $count
  * @return void
  */
-function log_rotate($filename, $size, $count)
+function log_rotate($logsDir, $count)
 {
-    $fileSize = @filesize($filename);
+    $it = new FilesystemIterator($logsDir, FilesystemIterator::CURRENT_AS_PATHNAME);
+    /**
+     * @var DateTime[]
+     */
+    $dateFiles = [];
 
-    if (false == $filename || $fileSize < $size) {
-        return;
-    }
+    foreach ($it as $filename) {
+        if (!$it->isFile()) {
+            continue;
+        }
 
-    for ($i = $count; $i > 0; --$i) {
-        $prevFilename = $filename . '.' . ($i - 1);
-        if (file_exists($prevFilename)) {
-            rename($prevFilename, "$filename.$i");
+        if (preg_match('/SFIC_([0-9]{2}-[0-9]{2}-[0-9]{4})\.log/', $filename, $matches) === false) {
+            continue;
+        }
+
+        $date = DateTime::createFromFormat('d-m-Y', $matches[1]);
+        $date->setTime(0, 0);
+
+        $inserted = false;
+        for ($i = 0, $fileCount = count($dateFiles); $i < $fileCount; ++$i) {
+            $dateFile = $dateFiles[$i];
+            if ($date < $dateFile) {
+                $dateFiles = array_splice($dateFiles, $i, 0, [$date]);
+
+                $inserted = true;
+                break;
+            }
+        }
+
+        if (!$inserted) {
+            $dateFiles[] = $date;
         }
     }
 
-    rename($filename, "$filename.1");
+    $fileCount = count($dateFiles);
+
+    if ($fileCount > $count) {
+        for ($i = $fileCount - 1; $i >= $count; --$i) {
+            $filename = $logsDir . '/SFIC_' . $dateFiles[$i]->format('d-m-Y') . '.log';
+        }
+    }
 }
